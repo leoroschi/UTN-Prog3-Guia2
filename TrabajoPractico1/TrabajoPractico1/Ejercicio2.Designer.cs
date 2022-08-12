f39d21f07ab801bab82b0676dcda1aca990012d3
Leoroschi <leoroschi@gmail.com>
2022-08-12 14:48:52 -0300

    Ejercicio 2 terminado

--- TrabajoPractico1/TrabajoPractico1/Ejercicio2.Designer.cs
+++ TrabajoPractico1/TrabajoPractico1/Ejercicio2.Designer.cs
@@ -29,12 +29,144 @@ namespace TrabajoPractico1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gbIngreso = new System.Windows.Forms.GroupBox();
+            this.gbElementos = new System.Windows.Forms.GroupBox();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.lbElementos = new System.Windows.Forms.ListBox();
+            this.gbIngreso.SuspendLayout();
+            this.gbElementos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbIngreso
+            // 
+            this.gbIngreso.Controls.Add(this.btnCancelar);
+            this.gbIngreso.Controls.Add(this.txtApellido);
+            this.gbIngreso.Controls.Add(this.txtNombre);
+            this.gbIngreso.Controls.Add(this.lblApellido);
+            this.gbIngreso.Controls.Add(this.lblNombre);
+            this.gbIngreso.Controls.Add(this.btnAgregar);
+            this.gbIngreso.Location = new System.Drawing.Point(33, 34);
+            this.gbIngreso.Name = "gbIngreso";
+            this.gbIngreso.Size = new System.Drawing.Size(344, 391);
+            this.gbIngreso.TabIndex = 0;
+            this.gbIngreso.TabStop = false;
+            this.gbIngreso.Text = "Ingreso de datos";
+            // 
+            // gbElementos
+            // 
+            this.gbElementos.Controls.Add(this.lbElementos);
+            this.gbElementos.Controls.Add(this.btnBorrar);
+            this.gbElementos.Location = new System.Drawing.Point(400, 34);
+            this.gbElementos.Name = "gbElementos";
+            this.gbElementos.Size = new System.Drawing.Size(358, 391);
+            this.gbElementos.TabIndex = 1;
+            this.gbElementos.TabStop = false;
+            this.gbElementos.Text = "Elementos";
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(143, 342);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(75, 23);
+            this.btnBorrar.TabIndex = 2;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(137, 211);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(31, 94);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(47, 13);
+            this.lblNombre.TabIndex = 4;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(31, 130);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(47, 13);
+            this.lblApellido.TabIndex = 5;
+            this.lblApellido.Text = "Apellido:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(84, 91);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(235, 20);
+            this.txtNombre.TabIndex = 6;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(84, 130);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(235, 20);
+            this.txtApellido.TabIndex = 7;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(137, 342);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 3;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // lbElementos
+            // 
+            this.lbElementos.FormattingEnabled = true;
+            this.lbElementos.Location = new System.Drawing.Point(30, 37);
+            this.lbElementos.Name = "lbElementos";
+            this.lbElementos.Size = new System.Drawing.Size(301, 290);
+            this.lbElementos.Sorted = true;
+            this.lbElementos.TabIndex = 3;
+            // 
+            // Ejercicio2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gbElementos);
+            this.Controls.Add(this.gbIngreso);
+            this.Name = "Ejercicio2";
             this.Text = "Ejercicio2";
+            this.gbIngreso.ResumeLayout(false);
+            this.gbIngreso.PerformLayout();
+            this.gbElementos.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gbIngreso;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.GroupBox gbElementos;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.ListBox lbElementos;
     }
 }
--- TrabajoPractico1/TrabajoPractico1/Ejercicio2.cs
+++ TrabajoPractico1/TrabajoPractico1/Ejercicio2.cs
@@ -16,5 +16,46 @@ namespace TrabajoPractico1
         {
             InitializeComponent();
         }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            bool repite = false;
+            foreach (string elemento in lbElementos.Items)
+            {
+                if (elemento == txtNombre.Text + " " + txtApellido.Text)
+                {
+                    repite = true;
+                }
+            }
+            if (repite == false && txtNombre.Text.Trim() != "" && txtApellido.Text.Trim() != "")
+            {
+                lbElementos.Items.Add(txtNombre.Text + " " + txtApellido.Text);
+                txtNombre.Text = "";
+                txtApellido.Text = "";
+                txtNombre.Focus();
+            }
+            else
+            {
+                if (repite == true) MessageBox.Show("El nombre agregado no puede repetirse");
+                else MessageBox.Show("Las casillas de Nombre y Apellido deben completarse");
+            }
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            if (lbElementos.SelectedItem != null)
+            {
+                lbElementos.Items.Remove(lbElementos.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show("Debe haber una selección para poder borrarla");
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
